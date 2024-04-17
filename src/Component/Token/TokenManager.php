cadff39beb8d72e612d063158b5af77ff5aef848
Karim Khakimov <shevabratan@gmail.com>
2024-04-17 11:45:08 +0500

    first commit

---
+++ src/Component/Token/TokenManager.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Component\Token;
+
+use App\Component\Core\AbstractManager;
+use App\Entity\Token;
+
+/**
+ * @method save(Token $entity, bool $needToFlush = false)
+ */
+class TokenManager extends AbstractManager
+{
+}
